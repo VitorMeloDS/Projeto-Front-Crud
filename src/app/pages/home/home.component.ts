09c02a066bbef38e7dcc4ee58c03e7d8fe04dcfe
Vitor Melo da Silva <vmsvitor20@gmail.com>
2022-10-18 00:40:41 -0300

    feat: add busca dos livros

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,3 +1,4 @@
+import { FormGroup, FormBuilder } from '@angular/forms';
 import { HomeServices } from './../../../services/home/home.service';
 import { Component } from '@angular/core';
 import { Book } from 'src/app/interfaces/book';
@@ -8,15 +9,21 @@ import { Book } from 'src/app/interfaces/book';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
-  protected books?: Book[];
+  protected books: Book[] = [];
   protected mostrar: boolean = false;
+  protected formSearchBook!: FormGroup;
 
   constructor(
-    private homeSvc: HomeServices
+    private homeSvc: HomeServices,
+    private formBuilder: FormBuilder
     ) { }
 
   // * Toda vez que a página é inicializada essa função busca os dados dos livros
   ngOnInit() {
+    this.formSearchBook = this.formBuilder.group({
+      search: ['']
+    });
+
     this.homeSvc.getAll('/book').subscribe({
       next: (data) => {
         this.books = data;
@@ -26,6 +33,22 @@ export class HomeComponent {
         alert(err.message);
       },
     })
+  }
+
+  protected search() {
+    let searchBook: Book[] = [];
+    if (this.formSearchBook.controls['search'].value) {
+      for (const book of this.books) {
+        if (book.titulo.toLocaleLowerCase().includes(this.formSearchBook.controls['search'].value.toLocaleLowerCase())){
+          searchBook?.push(book);
+        }
+      }
+    }
+    if (searchBook)
+      this.books = searchBook;
+    if (!this.formSearchBook.controls['search'].value) {
+      this.ngOnInit();
+    }
   }
 
 }
