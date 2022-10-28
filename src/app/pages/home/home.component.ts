db20fd7094bb4a14372433bfcf7e8dab236240ae
Vitor Melo da Silva <vmsvitor20@gmail.com>
2022-10-28 13:41:30 -0300

    feat: add loading and message for not found book

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -12,6 +12,8 @@ export class HomeComponent {
   protected books: Book[] = [];
   protected mostrar: boolean = false;
   protected formSearchBook!: FormGroup;
+  protected loading: boolean = true;
+  protected notSearchBook: boolean = false;
 
   constructor(
     private homeSvc: HomeServices,
@@ -29,11 +31,19 @@ export class HomeComponent {
   // * Search books
   protected async search() {
     let searchBook: Book[] = [];
+    this.loading = true;
+    this.notSearchBook = false;
+
     if (this.formSearchBook.controls['search'].value) {
       for (const book of this.books) {
         if (book.titulo.toLocaleLowerCase().includes(this.formSearchBook.controls['search'].value.toLocaleLowerCase())){
           searchBook?.push(book);
+          this.loading = false;
         }
+      }
+      if (searchBook.length == 0) {
+        this.loading = false;
+        this.notSearchBook = true;
       }
     }
     this.books = searchBook;
@@ -44,9 +54,12 @@ export class HomeComponent {
 
   // * Load Books for page
   private async loadBook() {
+
     this.homeSvc.getAll('/book').subscribe({
       next: (data) => {
         this.books = data;
+        this.loading = false;
+        this.notSearchBook = false;
       },
       error: (err) => {
         console.log(err.message);
