3c36d0df9f0f740479f78aeb77078ec94afa0dfc
Vitor Melo da Silva <96471272+VitorMeloDS@users.noreply.github.com>
2023-07-27 13:45:39 -0300

    Update config.ts

--- src/services/config.ts
+++ src/services/config.ts
@@ -2,7 +2,8 @@ export class Config {
   private baseUrl: string;
 
   constructor(){
-    this.baseUrl = 'https://rocky-woodland-82968.herokuapp.com';
+    // this.baseUrl = 'https://rocky-woodland-82968.herokuapp.com';
+    this.baseUrl = 'https://crud-mongodb-pweb-production.up.railway.app';
   }
 
   getApiDomain(){
